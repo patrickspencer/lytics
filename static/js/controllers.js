ab31b14f641742cf6465bb07643dae3e3e428ed5
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2016-01-11 00:34:07 -0500

    Update angular app

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,15 +1,34 @@
 var lyticsApp = angular.module('lyticsApp', []);
 
-lyticsApp.controller('ExpenditureCtrl', function($scope, $http) {
-    if ($scope.search == undefined) {
-        $scope.search = "Sherlock Holmes";
-        fetch();
-    }
-    function fetch() {
-        $http.get("http://www.omdbapi.com/?t=" + $scope.search +
-                  "&tomatoes=true&plot=full")
-            .success(function(response) {
-                $scope.details = response;
+lyticsApp.controller('CreateExpenditure', function($scope, $http) {
+
+
+    $scope.fetchExpenditures = function () {
+      $http.get("/api/expenditures/")
+        .success(function (response) {
+            $scope.expenditures = response.json_list;
+      });
+    };
+
+    $scope.createExpenditure = function () {
+        var data = {date: $scope.date, time: $scope.time,
+          description: $scope.description, cost: $scope.cost, category_id: ""};
+        $scope.data = data;
+
+        $http.post("/api/expenditures/", data)
+            .success(function (data, status, headers) {
+                $scope.fetchExpenditures();
+        });
+
+    }; // end createExpenditure callback
+
+    // callback for ng-click 'deleteUser':
+    $scope.delete = function (expenditure_id) {
+        $http.delete("/api/expenditure/" + expenditure_id + "/")
+            .success(function (data, status, headers) {
+                $scope.fetchExpenditures();
         });
     };
+
 });
+
