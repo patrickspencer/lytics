ea10fc3790c544ad47b2e9f334e7fa2d39d949df
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2016-01-13 04:02:41 -0500

    Refactor main module

---
+++ lytics/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+    lytics
+    ~~~~~~
+    Main file for lytics app.
+
+    Usage: python lyrics.py
+
+    :copyright: (c) 2016 by Patrick Spencer.
+    :license: Apache 2.0, see LICENSE for more details.
+"""
+
+from flask import Flask
+from flask import render_template
+from db import queries
+from api import make_api
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Blah'
+    # return render_template('index.jinja2')
+
+@app.route("/month/<year>/<month>")
+def month_view(year, month):
+    e = queries.get_expenditures_in_month(year,month)
+    return render_template('expenditures/month.jinja2',
+            expenditures=e)
+
+make_api(app)
+
+if __name__ == "__main__":
+    app.run(debug=True)
