44a0683356c7c031125f48a8d0d8e9aeef2df784
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2016-01-13 05:55:27 -0500

    Update queries test

--- tests/unit/test_queries.py
+++ tests/unit/test_queries.py
@@ -5,16 +5,23 @@ import os
 import tempfile
 import lytics
 
+# Set DATABASE_URI to testing database
+lytics.app.config.from_object('lytics.settings.TestingConfig')
+
+
 class TestQueriesModule(unittest.TestCase):
 
     def setup(self):
         lytics.app.config.from_object('lytics.settings.TestingConfig')
-        print(lytics.app.config['DATABASE_URI'])
 
-    def test_dbconn(self):
+
+    def test_create_expenditure(self):
         """
-        DBConn(DATABASE_PATH) should load a conenction to the db
+        Should have entry in database after create_expenditure is called
         """
+        # this creates the test database if it doesn't already exist
+        query_conn = lytics.db.queries.QueryConn(lytics.app.config['DATABASE_URI'])
+        lytics.db.queries.create_expenditure('2016-01-20','15:00','This is a decrition',5.20,1)
 
     def tearDown(self):
         pass
