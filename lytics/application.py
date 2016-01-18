5647f0b2a2c3a8f0a3bd085ab5bd5ec18b55fcdc
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2016-01-17 20:31:14 -0600

    Add application file

---
+++ lytics/application.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from lytics import settings
+from flask_restful import Resource, Api, reqparse
+from lytics.db import queries
+
+def create_app(config_filename):
+    """
+    Returns a Flask app given a configuration object
+
+    :param config_object: a string which points to a class holding the
+    configuration settings
+    """
+    app = Flask(__name__)
+    app.config.from_object(settings.DevelopmentConfig)
+
+    from lytics.routes import main
+
+    with app.app_context():
+        app.register_blueprint(main)
+    # api = Api(app)
+    # api.add_resource(ExpenditureList, '/api/expenditures/')
+    # api.add_resource(Expenditure, '/api/expenditure/<expenditure_id>/')
+
+    return app
