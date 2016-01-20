41b3e71da7eb9203c207c01beefb870f15cb5ee6
Patrick Spencer <patrick.spencer@mail.mizzou.edu>
2016-01-20 11:22:26 -0600

    Update tests

--- tests/unit/test_queries.py
+++ tests/unit/test_queries.py
@@ -7,27 +7,23 @@ import lytics
 from sqlalchemy import create_engine, inspect, desc, exists
 from sqlalchemy.orm import sessionmaker
 from lytics.db.models import Expenditure
-
-# Set DATABASE_URI to testing database
-lytics.app.config.from_object('lytics.settings.TestingConfig')
+from lytics import settings, db
+from lytics.db import queries
 
 class TestQueriesModule(unittest.TestCase):
-
-    def setup(self):
-        pass
 
     def test_create_expenditure(self):
         """
         Should have entry in database after create_expenditure is called
         """
         # this creates the test database if it doesn't already exist
-        query_conn = lytics.db.queries.QueryConn(lytics.app.config['DATABASE_URI'])
         # create_expenditure creates an expenditures and returns its id
+        query_conn = queries.QueryConn(settings.DATABASE_URI)
         expenditure_id = query_conn.create_expenditure('2016-01-20','15:00',
                 'This is a description',5.20,1)
 
         # connect to db to see if create_expenditure worked
-        engine = create_engine(lytics.app.config['DATABASE_URI'], echo=False)
+        engine = create_engine(settings.DATABASE_URI, echo=False)
         session = sessionmaker(bind=engine)
         Session = session()
         expenditure = Session.query(Expenditure).get(expenditure_id)
@@ -37,8 +33,46 @@ class TestQueriesModule(unittest.TestCase):
         Session.delete(expenditure)
         Session.commit()
 
-    def tearDown(self):
-        pass
+    def test_expenditure_exists(self):
+        """
+        Should return True if expenditure exists
+        """
+        # this creates the test database if it doesn't already exist
+        # create_expenditure creates an expenditures and returns its id
+        query_conn = queries.QueryConn(settings.DATABASE_URI)
+        expenditure_id = query_conn.create_expenditure('2016-01-20','15:00',
+                'This is a description',5.20,1)
 
+        # connect to db to see if create_expenditure worked
+        engine = create_engine(settings.DATABASE_URI, echo=False)
+        session = sessionmaker(bind=engine)
+        Session = session()
+        expenditure = Session.query(Expenditure).get(expenditure_id)
+        self.assertTrue(query_conn.expenditure_exists(expenditure_id))
+
+        # delete expenditure
+        Session.delete(expenditure)
+        Session.commit()
+
+    def test_get_expenditure_by_id(self):
+        """
+        Should return True if expenditure exists
+        """
+        # this creates the test database if it doesn't already exist
+        # create_expenditure creates an expenditures and returns its id
+        query_conn = queries.QueryConn(settings.DATABASE_URI)
+        expenditure_id = query_conn.create_expenditure('2016-01-20','15:00',
+                'This is a description',5.20,1)
+
+        # connect to db to see if create_expenditure worked
+        engine = create_engine(settings.DATABASE_URI, echo=False)
+        session = sessionmaker(bind=engine)
+        Session = session()
+        expenditure = Session.query(Expenditure).get(expenditure_id)
+        self.assertTrue(query_conn.expenditure_exists(expenditure_id))
+
+        # delete expenditure
+        Session.delete(expenditure)
+        Session.commit()
 if __name__ == '__main__':
     unittest.main()
